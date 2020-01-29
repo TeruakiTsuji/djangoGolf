c475e581cae9d19c32f34e19115bbd829a54ebaf
Tsuji.Teruaki <ttsuji@localhost.localdomain>
2020-01-29 21:51:14 +0900

    20200129

---
+++ next/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+import datetime
+from index import LocalDate
+from index import Sqlite
+
+# Create your views here.
+class NextTemplateView(TemplateView):
+
+    template_name = 'next.html'
+
+    def get(self, request, *args, **kwargs):
+
+        results = [];
+        dt_now = datetime.datetime.now()
+        today = dt_now.strftime('%Y%m%d')
+        result = Sqlite.execute('select * from next where open_dt >= \'' + today + '\' order by open_dt')
+
+        for row in result:
+        	wdate = LocalDate.wdatey(row[0])
+        	row = { 'open_dt' : wdate, 'place' : row[1] }
+        	results.append(row)
+
+        context = {
+            'app' : 'Next',
+            'results' : results
+        }
+        return render(self.request, self.template_name, context)
+
